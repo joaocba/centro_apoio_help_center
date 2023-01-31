5d885d63a3ae79c8f95bd731c8e2c8d1439517f7
Joao B <joaocapb1991@gmail.com>
2023-01-31 20:28:18 +0000

    upd

--- root/assets/js/scripts.js
+++ root/assets/js/scripts.js
@@ -2,13 +2,9 @@
 // Sidebar (abrir-fechar)
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
+    // Toggler
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
